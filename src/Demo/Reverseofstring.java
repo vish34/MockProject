4901b072df421fb8263f939ffa2f85963e0c8333
vishu <vishu@Vishwadeep>
2020-08-14 10:43:36 +0530

    This is my commit.

---
+++ src/Demo/Reverseofstring.java
@@ -0,0 +1,53 @@
+package Demo;
+
+public class Reverseofstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// To reverse the string
+     
+		//reversestring1.method1(); // By using array converter
+		reversestring1.method2(); // By using stringbuffer
+		
+	}
+	
+		public static class reversestring1 {
+			public static void method1()
+			{
+		
+				// 
+				String str = "Vishwadeep       peedawhsiV             ";
+				char chars[] = str.toCharArray();  // converted to character array and printed in reverse order
+				for(int i= chars.length-1; i>=0; i--) {
+					System.out.print(chars[i]);
+				}
+			}
+	
+			public static void method2() {
+				String str = "I am Software Tester.               .retseT erawtfoS ma I ";
+				int i = str.length();      // calculating total length of the string
+				StringBuffer strb = new StringBuffer();
+				for( int j=i-1; j>=0; j--)
+				{
+					strb = strb.append(str.charAt(j));
+				}
+				System.out.println(strb);
+			}
+		}
+	
+		public static class reverseno{
+			public void rmethod1() {
+				
+				
+				
+				
+			}
+			
+			
+			
+			
+		}
+		
+		
+	}
---
+++ src/Demo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author vishu
+ *
+ */
+package Demo;
